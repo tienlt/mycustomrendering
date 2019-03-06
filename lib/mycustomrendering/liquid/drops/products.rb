c027685c19e932d523b7d188c8318b23428c7783
tienlt <thuytienkn@gmail.com>
2019-03-06 14:37:28 +0700

    Update products.rb

--- lib/mycustomrendering/liquid/drops/products.rb
+++ lib/mycustomrendering/liquid/drops/products.rb
@@ -2,9 +2,29 @@ module Mycustomrendering
   module Liquid
     module Drops
       class Products < ::Liquid::Drop
+        def list1en()
+          "/en/blog1,/en/blog2,/en/blog3,/en/blog3,/en/blog4,/en/blog5"
+        end
 
+        def list1da()
+          "/da/blog1,/da/blog2,/da/blog3,/da/blog4,/da/blog4,/da/blog5"
+        end
 
+        def list2en()
+          "Flashtest Your Business,Become Ambassador to Product Data Lake,Free Trial,Stay Up to Date with the Latest News,Welcome,The Long Tail of Product Data Synchronization"
+        end
+
+        def list2da()
+          "The Long Tail of Product Data Synchronization,Hold dig ajour med den seneste udvikling,Gratis prøveperiode, Hvad kan vi?, Bliv Ambassadør for Product Data Lake, Lyntest Jeres virksomhed"
+        end
         
+        def list3en()
+          "Flashtest Your Business,Become Ambassador to Product Data Lake,Free Trial,Stay Up to Date with the Latest News,Welcome,The Long Tail of Product Data Synchronization"
+        end
+
+        def list3da()
+          "The Long Tail of Product Data Synchronization,Hold dig ajour med den seneste udvikling,Gratis prøveperiode, Hvad kan vi?, Bliv Ambassadør for Product Data Lake, Lyntest Jeres virksomhed"
+        end
       end
     end
   end
